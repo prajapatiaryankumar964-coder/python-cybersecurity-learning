638cfef893baf7f131e4c92604ce57a8cf99421f
PRAJAPATI ARYANKUMAR PRAKASHBHAI <prajapatiaryankumar964@gmail.com>
2026-09-03 18:08:18 +0530

    Add files via upload

---
+++ sc1.py
@@ -0,0 +1,7 @@
+user_input = input("Enter your password: ")
+
+
+if len(user_input) >= 8:
+    print('Strong Password') 
+else:
+    print('Weak Password') 
---
+++ sc2.py
@@ -0,0 +1,7 @@
+user_input = input("Enter your email id: ")
+
+if "@" in user_input:
+    print("Valid Email Format")
+else:
+    print("Invalid Email!")
+    
